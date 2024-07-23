52c7cc76620e66c3fccaf25cfeb7ef1ba864b194
DevMehta-30 <dev.a.mehta30@gmail.com>
2024-07-23 23:34:25 +0530

    checking for git auth

--- CODE/test.py
+++ CODE/test.py
@@ -1,9 +1,14 @@
 import os
-stream = os.popen('git add .')
-stream.read()
+try:
+    stream = os.popen('git add .')
+    stream.read()
 
-stream = os.popen(r'git commit -m "initial test" ')
-stream.read()
+    stream = os.popen(r'git commit -m "checking for git auth"')
+    stream.read()
 
-stream = os.popen('git push origin main')
-stream.read()
+    stream = os.popen('git push origin main')
+    stream.read()
+except:
+    text="GitHub not authenticated"
+    cmd = "echo {} ".format(text)
+    os.syetem(cmd)
