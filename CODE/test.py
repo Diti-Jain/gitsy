2ecdc99fe7e1788fad2467a006af471973c43e80
Diti-Jain <ditijainj@gmail.com>
2024-08-05 23:19:44 +0530

    basic push feature implemented

--- CODE/test.py
+++ CODE/test.py
@@ -1,12 +1,21 @@
 import os
 try:
+    try:
+
+        stream = os.popen('git checkout -b newbranch')
+        stream.read()
+    except:
+        stream = os.popen('git checkout newbranch')
+        stream.read()
+
+
     stream = os.popen('git add .')
     stream.read()
 
     stream = os.popen(r'git commit -m "basic push feature implemented"')
     stream.read()
 
-    stream = os.popen('git push origin main')
+    stream = os.popen('git push origin head')
     stream.read()
 except:
     text="GitHub not authenticated"
