075fb48ff2c974e388169d09dde0baad46631dff
Diti-Jain <ditijainj@gmail.com>
2024-08-06 14:20:55 +0530

    message

--- CODE/test.py
+++ CODE/test.py
@@ -1,25 +1,2 @@
-import os
-try:
-    try:
-
-        stream = os.popen('git checkout newbranch')
-        stream.read()
-    except:
-
-        stream = os.popen('git checkout -b newbranch')
-        stream.read()
-
-    stream = os.popen('git add .')
-    stream.read()
-
-    stream = os.popen(r'git commit -m "newbranch testing "')
-    stream.read()
-
-    stream = os.popen('git push origin head')
-    stream.read()
-
-
-except:
-    text="GitHub not authenticated"
-    cmd = "echo {} ".format(text)
-    os.syetem(cmd)
+import subprocess
+subprocess.run(["git", "checkout", "-b", 'second'], check=True)
