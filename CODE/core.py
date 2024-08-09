701757760211b00fbb7a5273f269b23f3ba614b6
Diti-Jain <ditijainj@gmail.com>
2024-08-09 19:06:50 +0530

    Modified: .gitignore, CODE/__pycache__/core.cpython-311.pyc, CODE/core.py, __pycache__/cli.cpython-311.pyc, cli.py

--- CODE/core.py
+++ CODE/core.py
@@ -6,12 +6,68 @@ class GitsyCommand:
     def __init__(self, repo_path):
         self.repo_path = repo_path
 
-    def add(self,file):
+    def automessage(self):
+        result=subprocess.run(['git','status','--porcelain'],stdout=subprocess.PIPE, stderr=subprocess.PIPE,text=True)
+        git_status_output=result.stdout.strip()
+        if not git_status_output:
+            return 'no changes to commit'
+        added = []
+        modified = []
+        deleted = []
+        renamed = []
+        untracked = []
+        others = []
 
-        subprocess.run(["git", "add"]+ file, check=True)
+        # Parse each line of the output
+        for line in git_status_output.splitlines():
+            arr=line.split()
+            status=arr[0]
+            file_name=arr[1]
+            if status == 'A':
+                added.append(file_name)
+            elif status in ['M', 'MM']:
+                modified.append(file_name)
+            elif status == 'D':
+                deleted.append(file_name)
+            elif status == 'R':
+                renamed.append(file_name)
+            elif status == '??':
+                untracked.append(file_name)
+            else:
+                others.append(f"{status}: {file_name}")
+
+        # Construct the commit message
+        commit_message_parts = []
+
+        if added:
+            commit_message_parts.append(f"Added: {', '.join(added)}")
+        if modified:
+            commit_message_parts.append(f"Modified: {', '.join(modified)}")
+        if deleted:
+            commit_message_parts.append(f"Deleted: {', '.join(deleted)}")
+        if renamed:
+            commit_message_parts.append(f"Renamed: {', '.join(renamed)}")
+        if untracked:
+            commit_message_parts.append(f"Untracked: {', '.join(untracked)}")
+        if others:
+            commit_message_parts.append(f"Other changes: {', '.join(others)}")
+        return "; ".join(commit_message_parts)
+
+
+
+
+
+    def add(self,file):
+        if file==None:
+            subprocess.run(['git','add','.'],check=True)
+        else:
+            subprocess.run(["git", "add"]+ file, check=True)
 
 
     def commit(self, message):
+        if message==None:
+            message=self.automessage()
+
         subprocess.run(["git", "commit", "-m", message], check=True)
 
     def push(self, message, branch, time1,file):
--- cli.py
+++ cli.py
@@ -8,11 +8,12 @@ def main():
     subparsers = parser.add_subparsers(dest='command',help="Subcommands")
 
     push_parser=subparsers.add_parser('push',help='Push changes to a branch')
-    push_parser.add_argument('message', help='The commit message')
+    push_parser.add_argument('-m','--message',default=None,  help='The commit message')
+
 
     push_parser.add_argument('-b','--branch',default= None ,  help='The branch to push to')
     push_parser.add_argument('-t','--time',default=time.strftime("%H:%M", time.localtime()), help='The time you want to push your commit')
-    push_parser.add_argument('-f','--file',nargs='+' ,default='.', help='Specific file name to push')
+    push_parser.add_argument('-f','--file',nargs='+' ,default=None, help='Specific file name to push')
     args = parser.parse_args()
     execute_command(args)
 
@@ -20,10 +21,11 @@ def execute_command(args):
     gs = GitsyCommand(os.getcwd())
     command = args.command
     if command == 'push':
-        file=args.file if args.file else '.'
+        message=args.message if args.message else None
+        file=args.file if args.file else None
         time=args.time if args.time else time.strftime("%H:%M", time.localtime())
         branch = args.branch if args.branch else None
-        gs.push(args.message,branch,time,file)
+        gs.push(message,branch,time,file)
         print("successful")
 
 if __name__ == "__main__":
