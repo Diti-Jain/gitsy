2335d542214b017c89a2fba37685df3ab16eb3b4
Diti-Jain <ditijainj@gmail.com>
2024-08-13 21:54:53 +0530

    Deleted: CODE/test.py, CODE/test2.py

--- CODE/test.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import sys
-result = subprocess.run(["git", "diff", "--cached", "--quiet"], check=True)
-if result.returncode == 0:
-    print("No changes staged for commit.")
-    sys.exit(0)
-
-git diff --name-only --cached
